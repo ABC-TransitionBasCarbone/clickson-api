417d376875446bcf8c776d4b29a8e9a842aaadf9
Chloe M <chloe.molle@neuf.fr>
2024-04-24 14:15:02 +0200

    add typescript

--- api/index.ts
+++ api/index.ts
@@ -6,8 +6,12 @@ const { VercelRequest, VercelResponse } = require("@vercel/node")
 // app.listen(3000, () => console.log("Server ready on port 3000."));
 
 app.get("/", async (req, res) => {
-    const users = await sql`SELECT * FROM users;`;
-    return res.status(200).json({ users: users.rows.map(u => u.name) });
+    try {
+        const users = await sql`SELECT * FROM users;`;
+        return res.status(200).json({ users: users.rows.map(u => u.name) });
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
 });
 
 
