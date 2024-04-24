1a1831e38859f900ecb972ceb71e0f164283e8f3
Chloe M <chloe.molle@neuf.fr>
2024-04-24 17:10:06 +0200

    fix vercel

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,10 @@
 const express = require("express");
 const app = express();
 const { sql } = require("@vercel/postgres");
-const { VercelRequest, VercelResponse } = require("@vercel/node")
-
-// app.listen(3000, () => console.log("Server ready on port 3000."));
 
 app.get("/", async (req, res) => {
-    try {
-        const users = await sql`SELECT * FROM users;`;
-        return res.status(200).json({ users: users.rows.map(u => u.name) });
-    } catch (e) {
-        res.status(500).json({ error: e })
-    }
+    const users = await sql`SELECT * FROM users;`;
+    return res.status(200).json({ users: users.rows.map(u => u.name) });
 });
-
 
 module.exports = app;
