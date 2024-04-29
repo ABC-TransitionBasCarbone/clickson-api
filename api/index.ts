a95dc34d26bafa2d857044d5ff591d4a368f5f88
Romain Crevecoeur <romain.crevecoeur@lilo.org>
2024-04-29 16:49:03 +0200

    add post signin

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const { sql } = require("@vercel/postgres");
+const port = 4000
 
 app.get("/", async (req, res) => {
-    const users = await sql`SELECT * FROM users;`;
-    return res.status(200).json({ users: users.rows.map(u => u.name) });
+  const users = await sql`SELECT * FROM users;`;
+  return res.status(200).json({ users: users.rows.map(u => u.name) });
+});
+
+
+app.post("/auth/signin", async (req, res) => {
+  const test = await sql`INSERT INTO public.users(
+        name, password)
+        VALUES (${req.body.name}, ${req.body.password});`;
+  console.log("🚀 ~ app.post ~ test:", test)
+
+  return res.status(201);
 });
 
 module.exports = app;
