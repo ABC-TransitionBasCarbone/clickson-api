40079a7f7a286c43ab4e16c74f9a86499b973a7b
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-08-20 11:55:46 +0200

    change user with emmission categories

--- api/endpoints/postgres.ts
+++ api/endpoints/postgres.ts
@@ -2,9 +2,9 @@ const { sql } = require("@vercel/postgres");
 
 module.exports = function (app) {
 
-    // app.get("/", async (req, res) => {
-    //     const users = await sql`SELECT * FROM users;`;
-    //     return res.status(200).json({ users: users.rows.map(u => u.name) });
-    // });
+    app.get("/", async (req, res) => {
+        const emissionCategories = await sql`SELECT * FROM emission_categories;`;
+        return res.status(200).json({ emissionCategories: emissionCategories.rows.map(e => e.label) });
+    });
 
 }
