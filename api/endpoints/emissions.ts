4a3c23434c4c246162efb3b8e51103ea858fd8f5
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-10-24 14:14:30 +0200

    maj package + maj FE

--- api/endpoints/emissions.ts
+++ api/endpoints/emissions.ts
@@ -7,11 +7,14 @@ module.exports = function (app: Application): void {
     app.get('/emission/categories/:id_language', async (req: Request, res: Response, next: NextFunction) => getEmissionCategoriesByLanguageId(req, res, next));
     app.get('/emission/sub-categories/:category_id', async (req: Request, res: Response, next: NextFunction) => getEmissionSubCategoriesByCategoryId(req, res, next));
     app.post('/emission/sub-categories', async (req: Request, res: Response, next: NextFunction) => getEmissionSubCategoriesByCategoryIds(req, res, next));
+    app.get('/emission-factors/:id_emission_sub_categorie', async (req: Request, res: Response, next: NextFunction) => getEmissionFactorsBySubCategorieId(req, res, next));
 
     async function getEmissionCategoriesByLanguageId(req: Request, res: Response, next: NextFunction) {
         try {
             const emission_categories = await sql`
-            select * from emission_categories where id_language=${req.params.id_language}`;
+            SELECT * 
+            FROM emission_categories 
+            WHERE id_language=${req.params.id_language}`;
             return res.status(200).json(emission_categories.rows);
         } catch (error) {
             return handleErrors(next, error);
@@ -21,7 +24,9 @@ module.exports = function (app: Application): void {
     async function getEmissionSubCategoriesByCategoryId(req: Request, res: Response, next: NextFunction) {
         try {
             const sub_categories = await sql`
-            select * from emission_sub_categories where id_emission_categorie=${req.params.category_id}`;
+            SELECT * 
+            FROM emission_sub_categories 
+            WHERE id_emission_categorie=${req.params.category_id}`;
             return res.status(200).json(sub_categories.rows);
         } catch (error) {
             return handleErrors(next, error);
@@ -32,9 +37,36 @@ module.exports = function (app: Application): void {
         try {
 
             const sub_categories = await sql.query(
-                `select * from emission_sub_categories 
-                where id in (${req.body.join(',')})`)
+                `SELECT * 
+                FROM emission_sub_categories 
+                WHERE id in (${req.body.join(',')})`)
             return res.status(200).json(sub_categories.rows);
+        } catch (error) {
+            return handleErrors(next, error);
+        }
+    }
+
+    async function getEmissionFactorsBySubCategorieId(req: Request, res: Response, next: NextFunction) {
+        try {
+            const emissions = await sql.query(
+                `SELECT 
+                    ef.id,
+                    ef.label,
+                    ef.value,
+                    ef.uncertainty,
+                    et.label AS type,
+                    eu.label AS unit
+                 FROM 
+                    emission_factors ef
+                 JOIN 
+                    emission_types et ON ef.id_type = et.id
+                 JOIN 
+                    emission_units eu ON ef.id_unit = eu.id
+                 WHERE 
+                    ef.id_emission_sub_categorie = $1`,
+                [req.params.id_emission_sub_categorie]
+            );
+            return res.status(200).json(emissions.rows);
         } catch (error) {
             return handleErrors(next, error);
         }
