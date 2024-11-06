19d872c398c684ddc0656c64ed99626ee4fc0ac4
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-11-06 10:38:10 +0100

    prisma seed functionnal

--- prisma/fixed_data/languages.ts
+++ prisma/fixed_data/languages.ts
@@ -1,42 +1,42 @@
 export const LANGUAGES = [
     {
         id: 1,
-        language_code: 'en',
-        flag_url: 'https://flagcdn.com/h20/gb.png',
+        languageCode: 'en',
+        flagUrl: 'https://flagcdn.com/h20/gb.png',
     },
     {
         id: 2,
-        language_code: 'fr',
-        flag_url: 'https://flagcdn.com/h20/fr.png',
+        languageCode: 'fr',
+        flagUrl: 'https://flagcdn.com/h20/fr.png',
     },
     {
         id: 3,
-        language_code: 'es',
-        flag_url: 'https://flagcdn.com/h20/es.png',
+        languageCode: 'es',
+        flagUrl: 'https://flagcdn.com/h20/es.png',
     },
     {
         id: 4,
-        language_code: 'it',
-        flag_url: 'https://flagcdn.com/h20/it.png',
+        languageCode: 'it',
+        flagUrl: 'https://flagcdn.com/h20/it.png',
     },
     {
         id: 5,
-        language_code: 'gr',
-        flag_url: 'https://flagcdn.com/h20/gr.png',
+        languageCode: 'gr',
+        flagUrl: 'https://flagcdn.com/h20/gr.png',
     },
     {
         id: 6,
-        language_code: 'hr',
-        flag_url: 'https://flagcdn.com/h20/hr.png',
+        languageCode: 'hr',
+        flagUrl: 'https://flagcdn.com/h20/hr.png',
     },
     {
         id: 7,
-        language_code: 'hu',
-        flag_url: 'https://flagcdn.com/h20/hu.png',
+        languageCode: 'hu',
+        flagUrl: 'https://flagcdn.com/h20/hu.png',
     },
     {
         id: 8,
-        language_code: 'ro',
-        flag_url: 'https://flagcdn.com/h20/ro.png',
+        languageCode: 'ro',
+        flagUrl: 'https://flagcdn.com/h20/ro.png',
     },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,47 @@
 
 import { PrismaClient } from '@prisma/client'
+import { EMISSIONCATEGORIES } from './fixed_data/emission-categories'
 import { EMISSIONFACTORS } from './fixed_data/emission-factors'
+import { EMISSIONSUBCATEGORIES } from './fixed_data/emission-sub-categories'
+import { EMISSIONTYPES } from './fixed_data/emission-types'
+import { EMISSIONUNITS } from './fixed_data/emission-units'
+import { LANGUAGES } from './fixed_data/languages'
 
 const prisma = new PrismaClient()
 
+const languages = async () => {
+    await prisma.emissionFactors.deleteMany()
+    await prisma.emissionUnits.deleteMany()
+    await prisma.emissionTypes.deleteMany()
+    await prisma.emissionSubCategories.deleteMany()
+    await prisma.emissionCategories.deleteMany()
+    await prisma.languages.deleteMany()
+    await prisma.languages.createMany({ data: LANGUAGES })
+}
+const emissionCategories = async () => {
+    await prisma.emissionSubCategories.deleteMany()
+    await prisma.emissionCategories.deleteMany()
+    await prisma.emissionCategories.createMany({ data: EMISSIONCATEGORIES })
+}
+const emissionSubCategories = async () => {
+    await prisma.emissionSubCategories.deleteMany()
+    await prisma.emissionSubCategories.createMany({ data: EMISSIONSUBCATEGORIES })
+}
+const emissionTypes = async () => {
+    await prisma.emissionTypes.deleteMany()
+    await prisma.emissionTypes.createMany({ data: EMISSIONTYPES })
+}
+const emissionUnits = async () => {
+    await prisma.emissionUnits.deleteMany()
+    await prisma.emissionUnits.createMany({ data: EMISSIONUNITS })
+}
 const emissionFactors = async () => {
     await prisma.emissionFactors.deleteMany()
     await prisma.emissionFactors.createMany({ data: EMISSIONFACTORS })
 }
 
 const main = async () => {
-    await Promise.all([emissionFactors])
+    await Promise.all([languages(), emissionCategories(), emissionSubCategories(), emissionTypes(), emissionUnits(), emissionFactors()])
 }
 
 main()
