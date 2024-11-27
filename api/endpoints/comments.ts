49866196fd4a075f9772ec84a678a1314f89c7e2
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-11-27 14:02:01 +0100

    add remove comment

--- api/endpoints/comments.ts
+++ api/endpoints/comments.ts
@@ -6,17 +6,30 @@ const prisma = new PrismaClient()
 
 module.exports = function (app: Application): void {
     app.post('/comments', createComment);
+    app.delete('/comments', deleteComment);
 
     async function createComment(req: Request, res: Response, next: NextFunction) {
         const { idEmissionSubCategory, comment } = req.body
         try {
-            const comments = await prisma.comments.create({
+            const commentToReturn = await prisma.comments.create({
                 data: { idEmissionSubCategory, comment }
             })
-            return res.status(200).json(comments);
+            return res.status(200).json(commentToReturn);
         } catch (error) {
             return handleErrors(next, error);
         }
     }
+
+    async function deleteComment(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.body
+        try {
+            const comment = await prisma.comments.delete({ where: { id } })
+
+            return res.status(200).json(comment);
+        } catch (error) {
+            return handleErrors(next, error);
+        }
+    }
+
 
 }
