8610875d0f540bbf055c9227e35915edf53f7a02
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2025-01-15 16:28:05 +0100

    update FE

--- api/endpoints/emissions.ts
+++ api/endpoints/emissions.ts
@@ -8,6 +8,9 @@ module.exports = function (app: Application): void {
     app.get('/emission/categories/:id_language', getEmissionCategories);
     app.get('/emission/sub-categories/:id_category', getEmissionSubCategoriesById);
     app.get('/emission-factors/:id_emission_sub_category', getEmissionFactors);
+    app.put('/emission-factors', updateEmissionFactor);
+    app.post('/emission-factors', createEmissionFactor);
+    app.get('/emission-factors', getAllEmissionFactors);
 
     async function getEmissionCategories(req: Request, res: Response, next: NextFunction) {
         try {
@@ -42,6 +45,78 @@ module.exports = function (app: Application): void {
             })
 
             return res.status(200).json(emissions);
+        } catch (error) {
+            return handleErrors(next, error);
+        }
+    }
+
+    async function getAllEmissionFactors(req: Request, res: Response, next: NextFunction) {
+        try {
+            const emissions = await prisma.emissionCategories.findMany({
+                select: {
+                    id: true,
+                    label: true,
+                    emissionSubCategories: {
+                        include: {
+                            emissionFactors: true
+                        }
+                    }
+                }
+            })
+
+            return res.status(200).json(emissions);
+        } catch (error) {
+            return handleErrors(next, error);
+        }
+    }
+
+    async function updateEmissionFactor(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id, value, uncertainty, depreciationPeriod, label } = req.body;
+            console.log("🚀 ~ updateEmissionFactor ~ req.body:", req.body)
+            const emission = await prisma.emissionFactors.update({
+                where: { id: id },
+                data: {
+                    value: value,
+                    uncertainty: uncertainty,
+                    depreciationPeriod: depreciationPeriod,
+                    label: label,
+                    unit: req.body.unit,
+                    type: req.body.type,
+                }
+            })
+
+            return res.status(200).json(emission);
+        } catch (error) {
+            return handleErrors(next, error);
+        }
+    }
+
+    async function createEmissionFactor(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { value, uncertainty, depreciationPeriod, label } = req.body;
+            const emission = await prisma.emissionFactors.create({
+                data: {
+                    value: value,
+                    uncertainty: uncertainty,
+                    depreciationPeriod: depreciationPeriod,
+                    label: label,
+                    unit: req.body.unit,
+                    type: req.body.type,
+                    emissionSubCategory: {
+                        connect: {
+                            id: req.body.idEmissionSubCategory
+                        }
+                    },
+                    language: {
+                        connect: {
+                            id: req.body.idLanguage
+                        }
+                    }
+                }
+            })
+
+            return res.status(200).json(emission);
         } catch (error) {
             return handleErrors(next, error);
         }
