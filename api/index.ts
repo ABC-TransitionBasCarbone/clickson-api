e35bdf004d6c8b7a8b4eb24881453af6379fffd9
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2025-04-15 15:38:05 +0200

    change cors

--- api/index.ts
+++ api/index.ts
@@ -22,7 +22,7 @@ app.use(cors({
         "http://localhost:3000",
         "http://localhost:8080",
         "https://clickson-tau.vercel.app",
-        "https://clickson-api-git-develop-abc-transitionbascarbones-projects.vercel.app",
+        "https://clickson-git-develop-abc-transitionbascarbones-projects.vercel.app",
         "https://calculator.clickson.eu"
     ]
 }));
